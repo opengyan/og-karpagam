bd7fbfdae08875e34fa1123170464f6716d3fb45
dineshkce19 <dineshnkce@gmail.com>
2016-08-23 10:19:31 +0530

    changes in files of week1

--- dinesh/week1pgms/overlappingIntervals.c
+++ dinesh/week1pgms/overlappingIntervals.c
@@ -1,21 +1,36 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-class RemoveDuplicates
+#include <stdio.h>
+int cmpfunc (const void * a, const void * b)
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Set obj = new HashSet();
-		Scanner input = new Scanner(System.in);
-		int size;
-		size = input.nextInt();
-		while( size> 0 ){
-			obj.add(input.nextInt());	
-			size--;			
-		}
-		Iterator x;
-		for(x = obj.iterator() ; x.hasNext() ; ){
-			System.out.println(x.next());
+   return ( *(int*)a - *(int*)b );
+}
+int main(void) {
+	// your code goes here
+	int size;
+	scanf("%d",&size);
+	int i,j,x[size],y[size];
+	for(i=0;i<size;i++){
+		scanf("%d%d",&x[i],&y[i]);
+	}
+	qsort(x,size,sizeof(int),cmpfunc);
+	qsort(y,size,sizeof(int),cmpfunc);
+	int flag=1;
+		for(i=0;i<size;i++){
+		if(y[i]<x[i+1]&&flag)
+			printf("%d %d\n",x[i],y[i]);
+		else{
+			if(x[i]>=y[i-1]&&flag&&x[i+1]<y[i]){
+				printf("%d ",x[i-1]);
+				flag=0;
+			}
+			if(flag==0){
+				if(y[i]<x[i+1]){
+					printf("%d\n",y[i]);
+					flag=1;
+				}
+			}
 		}
 	}
+	if(flag==0)
+		printf("%d\n",y[size-1]);
+	return 0;
 }
