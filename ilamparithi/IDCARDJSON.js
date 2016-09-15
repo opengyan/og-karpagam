ad17638bce245e89741036961f7f08392dfd2c6f
ip-ilamparithi <ilamparithi@outlook.com>
2016-09-15 11:34:21 +0530

    Create IDCARDJSON.js

---
+++ ilamparithi/IDCARDJSON.js
@@ -0,0 +1,18 @@
+{
+	"RollNo": "14N209",
+	"name": "K.ILAMPARITHI",
+	"Department": "EIE",
+	"Section": "B",
+	"Duration": "2014-2018",
+	"BloodGroup": "A+ve",
+	"DOB": "18-06-1996",
+	"ADDRESS": [{
+		"MotherName": "K.MAATHINI",
+		"line 1": "7/26,Adthiya Nagar,Premier Nagar",
+		"line 2": "Othakalamandapam",
+		"line 3": "Coimbatore-641032",
+		"line 4": "TAMILNADU",
+		"PhoneNumber": "9500297725"
+	}],
+	"Contact no": "9487363520"
+}
