d22705e7e4dd0ea00edc0a65a086f10f3025d742
Dinesh Raja .N <dineshnkce@gmail.com>
2016-09-23 20:47:46 +0530

    Create Node.java

---
+++ dinesh/week3pgms/priority-queues-using-linked-list/Node.java
@@ -0,0 +1,25 @@
+public class Node {
+	private int element;
+	private int priority;
+	public Node(int element, int priority) {
+		super();
+		this.element = element;
+		this.priority = priority;
+	}
+	public Node() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getElement() {
+		return element;
+	}
+	public void setElement(int element) {
+		this.element = element;
+	}
+	public int getPriority() {
+		return priority;
+	}
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	
+}
